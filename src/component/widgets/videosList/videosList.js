765728516c9074ca3d7c5c40f4cb69f34b0eab92
Aamir <ac4600@gmail.com>
2019-06-16 21:45:42 +0530

    requst function added

--- src/component/widgets/videosList/videosList.js
+++ src/component/widgets/videosList/videosList.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 import { URL } from '../../../config';
 import Button from '../Buttons/button';
+import { stringTypeAnnotation } from '@babel/types';
 
 export default class VideosList extends Component {
   state = {
@@ -12,6 +13,47 @@ export default class VideosList extends Component {
     start: this.props.start,
     end: this.props.start + this.props.amount,
     amount: this.props.amount,
+  };
+
+  componentWillMount() {
+    this.request(this.props.start, this.props.end);
+  }
+
+  request = (start, end) => {
+    //videos request not working
+    axios.get(`${URL}/videos?_start=${start}&_end=${end}`).then(response => {
+      console.log(response);
+      this.setState({
+        videos: response.data,
+      });
+    });
+    if (this.state.teams.length < 1) {
+      axios.get(`${URL}/teams`).then(response => {
+        console.log(response);
+        this.setState({
+          teams: response.data,
+        });
+      });
+    }
+  };
+
+  renderVideos = type => {
+    let template = null;
+    switch (type) {
+      case 'card':
+        template = this.state.teams.map((item, i) => {
+          return (
+            <div key={i}>
+              <p>{item.name}</p>
+            </div>
+          );
+        });
+        break;
+      default:
+        template = null;
+    }
+
+    return template;
   };
 
   loadMore = () => {};
@@ -33,9 +75,11 @@ export default class VideosList extends Component {
   };
 
   render() {
+    // console.log(this.state);
     return (
       <div className={Styles.videosList_wrapper}>
         {this.renderTitle(this.props.title)}
+        {this.renderVideos(this.props.type)}
         {this.renderButton(this.props.loadmore)}
       </div>
     );
